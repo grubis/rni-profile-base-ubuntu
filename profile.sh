96b128b7b2ba333ab41be0e64077f315e6ab8dbb
grubis <grubis@basementubuntu>
2020-05-19 18:41:56 -0500

    another typo

--- profile.sh
+++ profile.sh
@@ -40,8 +40,8 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
 		apt update && \
         tasksel install ${ubuntu_bundles} && \
         apt install -y ${ubuntu_packages} && \
-        dmidecode -s system-uuid | sed 's:-::g' > /etc/iotedge/uuid.txt
-        dmidecode -s system-serial-number > /etc/iotedge/serial.txt
+        dmidecode -s system-uuid | sed 's:-::g' > /etc/iotedge/uuid.txt && \
+        dmidecode -s system-serial-number > /etc/iotedge/serial.txt && \
         sed -i \"s#<SYMMETRIC_KEY>#\$(</etc/iotedge/uuid.txt sed 's/[\&/]/\\&/g')#g\" /etc/iotedge/config.yaml && \
 	   	sed -i \"s#<REGISTRATION_ID>#\$(</etc/iotedge/serial.txt sed 's/[\&/]/\\&/g')#g\" /etc/iotedge/config.yaml\"'" \
      ${PROVISION_LOG}  
