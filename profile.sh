3bdcbcb97da3ad95d6cad6b9ab2db27ecad60136
grubis <grubis@basementubuntu>
2020-05-19 07:51:54 -0500

    try again

--- profile.sh
+++ profile.sh
@@ -43,9 +43,9 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
         systemctl stop iotedge && \
         export SN=$(dmidecode -s system-serial-number) && \
         export UUID=$(dmidecode -s system-uuid) && \
-    	export UUID=${UUID//-} && \
-    	sed -i \"s#<SYMMETRIC_KEY>#${UUID}#g\" /etc/iotedge/config.yaml && \
-	   	sed -i \"s#<REGISTRATION_ID>#${SN}#g\" /etc/iotedge/config.yaml\"'" \
+    	export UUID=${\"UUID\"//-} && \
+    	sed -i \"s#<SYMMETRIC_KEY>#${\"UUID\"}#g\" /etc/iotedge/config.yaml && \
+	   	sed -i \"s#<REGISTRATION_ID>#${\"SN\"}#g\" /etc/iotedge/config.yaml\"'" \
      ${PROVISION_LOG}  
 #   wget --header \"Authorization: token ${param_token}\" -O - ${param_bootstrapurl}/conf/iotagentconfig.yaml > /etc/iotedge/iotagentconfig.yaml && \    
 #		sed -i \"s#<SYMMETRIC_KEY>#$UUID#g\" /etc/iotedge/config.yaml && \
