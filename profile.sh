385d6092635eaed9570e14cbb3783b6f79331d4e
grubis <grubis@basementubuntu>
2020-05-20 09:02:40 -0500

    d

--- profile.sh
+++ profile.sh
@@ -42,7 +42,7 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
         apt install -y ${ubuntu_packages} && \
         dmidecode -s system-uuid | sed 's:-::g' > /etc/iotedge/uuid.txt && \
         dmidecode -s system-serial-number > /etc/iotedge/serial.txt && \
-        sed -i \"s#<REGISTRATION_ID>#\$(</etc/iotedge/serial.txt sed 's/[\&/]/\\\\&/g')#g\" /etc/iotedge/config.yaml\"'" \
+        sed -i \"s#<REGISTRATION_ID>#\$(</etc/iotedge/serial.txt sed 's/[\\\&/]/\\\\\&/g')#g\" /etc/iotedge/config.yaml\"'" \
      ${PROVISION_LOG}  
 #       sed -i \"s#<SYMMETRIC_KEY>#\$(</etc/iotedge/uuid.txt sed 's/[\\&/]/\\\\&/g')#g\" /etc/iotedge/config.yaml && \
 #	   	sed -i \"s#<REGISTRATION_ID>#\$(</etc/iotedge/serial.txt sed 's/[\\&/]/\\\\&/g')#g\" /etc/iotedge/config.yaml\"'" \
