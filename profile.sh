8f4c22c6f04d8e8c903e2b627145be4e46e0083d
grubis <matthew.grubis@med.ge.com>
2020-05-27 17:57:11 -0400

    Update profile.sh
    Moved the serial check outside of the docker command.

--- profile.sh
+++ profile.sh
@@ -48,13 +48,15 @@ run "Installing Extra Packages on Ubuntu ${param_ubuntuversion}" \
 		chmod 777 /var/lib/iotedge/mgmt.sock && \
 		chmod 777 /var/lib/iotedge/workload.sock && \
         dmidecode -s system-uuid | sed 's:-::g' > /etc/iotedge/uuid.txt && \
-        dmidecode -s system-serial-number > /etc/iotedge/serial.txt && \
-	if [[ -z $(grep '[^[:space:]]' /etc/iotedge/serial.txt) ]]; then cut -c 1-8 /etc/iotedge/uuid.txt > /etc/iotedge/serial.txt; fi\"'" \
+        dmidecode -s system-serial-number > /etc/iotedge/serial.txt \"'" \
      ${PROVISION_LOG}  
 
 echo "Applying IoT Configuration" > dev/tty0
 rm -f $ROOTFS/etc/iotedge/config.yaml
 wget --header \"Authorization: token ${param_token}\" -O - ${param_bootstrapurl}/conf/iotagentconfig.yaml > $ROOTFS/etc/iotedge/config.yaml
+if [[ -z $(grep '[^[:space:]]' $ROOTFS/etc/iotedge/serial.txt) ]]; then
+  cut -c 1-8 $ROOTFS/etc/iotedge/uuid.txt > $ROOTFS/etc/iotedge/serial.txt
+fi
 sed -i "s#<SYMMETRIC_KEY>#$(<$ROOTFS/etc/iotedge/uuid.txt sed 's/[\&/]/\\&/g')#g" $ROOTFS/etc/iotedge/config.yaml
 sed -i "s#<REGISTRATION_ID>#$(<$ROOTFS/etc/iotedge/serial.txt sed 's/[\&/]/\\&/g')#g" $ROOTFS/etc/iotedge/config.yaml
 sed -i "s#<SCOPE_ID>#${param_azurescopeid}#g" $ROOTFS/etc/iotedge/config.yaml
